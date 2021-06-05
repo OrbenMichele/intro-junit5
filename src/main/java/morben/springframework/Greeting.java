3f22d940efe702ed2be5fc7f8e80888f7e956f85
OrbenMichele <micheleorben@gmail.com>
2021-06-05 20:58:16 -0300

    Maven tests

--- src/main/java/morben/springframework/Greeting.java
+++ src/main/java/morben/springframework/Greeting.java
@@ -4,7 +4,7 @@ package morben.springframework;
  * Created by micheleorben on 6/5/21
  */
 
-public class Greating {
+public class Greeting {
 
     private static final String HELLO = "Hello";
     private static final String WORLD = "World";
